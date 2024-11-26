5fa53104ab1e5db3b12ace2e3ae9a5dbc6f25bf4
Administrator <Administrator@SC-202001072038>
2024-11-26 21:58:03 +0800

    加入專案檔案。

---
+++ 2024_1030_wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2024_1030_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
